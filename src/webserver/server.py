9e55ba1f131fbf1ca31a3fa5c913bde3c74c35da
Sachin Bhurke <33746085+Sachinbhurke@users.noreply.github.com>
2025-05-06 14:24:46 -0700

    framework code added

---
+++ src/webserver/server.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from src.webserver.constants import *
+from pydantic import BaseModel
+import logging
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class RequestPayload(BaseModel):
+    model: str
+    prompt: str
+    images: list[str]
+    session_id: str
+
+# Initialize the FastAPI app
+app = FastAPI(
+    title="LLM inference API",
+    description="API for LLM inference",
+    version="0.1",
+)
+
+@app.get("/")
+def read_root():
+    return {"version": os.getenv("WEB_SERVER_VERSION")}
+
+@app.post("/api/generate")
+def generate_response(request: RequestPayload):
+    """Generate a response from the LLM model."""
+    logging.info(f"Received request: {request}")    
+    response = {}
+    return {"response": response}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("src.webserver.server:app", host="0.0.0.0", port=8000, reload=False)
